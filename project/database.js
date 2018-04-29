5634a7aaa7940389311288e5ef84f8e731a32f7e
askflow1111 <askflow1111@gmail.com>
2018-04-29 09:59:18 -0400

    added ibm visual api

---
+++ project/database.js
@@ -0,0 +1,21 @@
+var database = {
+  age: {
+    min: 48,
+    max: 51,
+    score: 0.8574606
+  },
+  face_location: {
+    height: 118,
+    width: 97,
+    left: 166,
+    top: 60
+  },
+  gender: {
+    gender: "MALE",
+    score: 0.99999356
+  }
+}
+
+module.exports = {
+  database,
+};
--- project/routes/index.js
+++ project/routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const visualRecognitionHelper = require('../services/ibm/visualRecognitionHelper');
 
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
-router.get('/camera', function(req, res, next) {
+router.get('/camera', function (req, res, next) {
   res.render('camera');
 });
 
-router.get('/detail', function(req, res, next) {
+router.get('/detail', visualRecognitionHelper.faceDetect, function (req, res, next) {
   res.render('detail');
 });
 
